a533fd654087efaaa6268590e7bb141ef58665b5
Justin D Kendall <jkendall@xpanxion.com>
2020-09-03 23:05:50 -0500

    Create placeholder Todo API service in frontend

---
+++ frontend/src/app/domain/todoEntry.ts
@@ -0,0 +1,7 @@
+export interface TodoEntry {
+  id: number;
+  title: string;
+  description: string;
+  createdAt: string;
+  completed: boolean;
+}
--- frontend/src/app/services/todo-api.service.ts
+++ frontend/src/app/services/todo-api.service.ts
@@ -1,10 +1,36 @@
 import {Injectable} from '@angular/core';
+import {TodoEntry} from '../domain/todoEntry';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TodoApiService {
 
+  private todos: TodoEntry[] = [];
+
   constructor() {
+  }
+
+  async createTodo(todoEntry: TodoEntry): Promise<boolean> {
+    this.todos.push(todoEntry);
+    return true;
+  }
+
+  async getTodoList(): Promise<TodoEntry[]> {
+    return this.todos;
+  }
+
+  async getTodoEntry(entryId: number): Promise<TodoEntry> {
+    const entry = this.todos.find(todo => todo.id === entryId);
+    return entry || Promise.reject('Entry not found');
+  }
+
+  async deleteTodo(entryId: number): Promise<void> {
+    if (!this.todos.some(todo => todo.id === entryId)) {
+      return Promise.reject('Entry not found');
+    }
+
+    this.todos = this.todos.filter(todo => todo.id !== entryId);
+    return;
   }
 }
