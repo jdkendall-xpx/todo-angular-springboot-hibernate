126b2abce945ee9e7f9146430aa819c9cc754dd0
Justin D Kendall <jkendall@xpanxion.com>
2020-09-08 23:57:54 -0500

    More styling & add edit button on hover over to-do card

--- frontend/src/app/services/todo-api.service.ts
+++ frontend/src/app/services/todo-api.service.ts
@@ -7,15 +7,33 @@ import {TodoEntry} from '../domain/todoEntry';
 export class TodoApiService {
 
   private todos: TodoEntry[] = [
-    {completed: false, createdAt: new Date('2020-09-08T07:27:49.000Z'), description: 'Here be A', id: 1, title: 'A'},
-    {completed: true, createdAt: new Date('2020-09-03T11:54:33.000Z'), description: 'Thar be B', id: 2, title: 'B'},
-    {completed: true, createdAt: new Date('2020-09-03T12:16:43.000Z'), description: 'Whar be C?', id: 3, title: 'C'},
     {
+      id: 1,
+      createdAt: new Date('2020-09-08T07:27:49.000Z'),
+      title: 'Attraction doesn’t spiritually reject any ego — but the cow is what exists.',
       completed: false,
-      createdAt: new Date('2020-08-31T11:01:10.000Z'),
-      description: 'Hardy har dee D!',
+      description: 'Private places of moonlight will compassionately follow a small spirit. All unprepared scholars trap each other, only holographic winds have a mind. The body studies fear which is not inner.',
+    },
+    {
+      id: 2,
+      createdAt: new Date('2020-09-03T11:54:33.000Z'),
+      title: 'Bromiums sunt magisters de teres buxum.',
+      completed: true,
+      description: 'Candidatus, habitio, et vox. Armariums nocere in primus vierium! Ubi est regius agripeta?',
+    },
+    {
+      id: 3,
+      createdAt: new Date('2020-09-03T12:16:43.000Z'),
+      title: 'When heating chilled chicken breasts, be sure they are room temperature.',
+      completed: true,
+      description: 'Peanuts can be soaked with salted watermelon, also try whisking the porridge with anchovy essence. Try mashing up the BBQ sauce chilis with crushed lime and red wine, refrigerated. After boiling the oysters, garnish popcorn, asparagus and sour milk with it in a grinder.',
+    },
+    {
       id: 4,
-      title: 'D'
+      createdAt: new Date('2020-08-31T11:01:10.000Z'),
+      title: 'Cloudy beauties lead to the mankind.',
+      completed: false,
+      description: 'The queen harvests sensor like a gravimetric green people. Kahlesses are the mermaids of the terrifying future. The sun is more nanomachine now than space suit. unrelated and always clear.',
     },
   ];
 
