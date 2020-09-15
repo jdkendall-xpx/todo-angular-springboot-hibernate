8b0e6d1f3f830998f5f7fc7415169dc971cf7f17
Justin D Kendall <jkendall@xpanxion.com>
2020-09-15 18:13:03 -0500

    Move creating new to-dos into a dialog widget on the main page

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {HomeComponent} from './pages/home/home.component';
 import {EditTodoComponent} from './pages/edit-todo/edit-todo.component';
-import {CreateTodoComponent} from './pages/create-todo/create-todo.component';
+import {CreateTodoComponent} from './pages/home/widgets/create-todo-dialog/create-todo.component';
 
 const routes: Routes = [
   {path: 'create', component: CreateTodoComponent},
