7e8a7a9fbba7d75d3ba461e0aecc7ac9cd2076a1
Justin D Kendall <jkendall@xpanxion.com>
2020-09-15 18:37:57 -0500

    Add UI button to create new to-do

--- frontend/src/app/pages/home/home.component.ts
+++ frontend/src/app/pages/home/home.component.ts
@@ -1,4 +1,6 @@
 import {Component, OnInit} from '@angular/core';
+import {MatDialog, MatDialogConfig} from '@angular/material/dialog';
+import {CreateTodoDialogComponent} from './widgets/create-todo-dialog/create-todo-dialog.component';
 
 @Component({
   selector: 'app-home',
@@ -7,10 +9,18 @@ import {Component, OnInit} from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() {
+  constructor(private dialog: MatDialog) {
   }
 
   ngOnInit(): void {
   }
 
+  openDialog(): void {
+    const dialogConfig = new MatDialogConfig();
+
+    dialogConfig.disableClose = true;
+    dialogConfig.autoFocus = true;
+
+    this.dialog.open(CreateTodoDialogComponent, dialogConfig);
+  }
 }
