50cfb326914d10f578c83de24a60eb809078759f
Justin D. Kendall <jkendall@xpanxion.com>
2020-10-19 12:44:12 -0500

    Refactored multiple todo methods as an example

--- backend/src/main/java/com/xpanxion/todo/services/TodoEntryService.java
+++ backend/src/main/java/com/xpanxion/todo/services/TodoEntryService.java
@@ -13,6 +13,26 @@ public class TodoEntryService {
     @Autowired
     TodoRepository todoRepository;
 
+    public void deleteTodo(long id) throws InvalidIdException {
+        Optional<TodoEntry> originalEntry = this.todoRepository.findById(id);
+
+        // Check if we found an entry
+        if (originalEntry.isPresent()) {
+            // Delete the entry we found by ID
+            this.todoRepository.deleteById(id);
+        } else {
+            // Throw an invalid ID exception since we didn't find the entry
+            throw new InvalidIdException();
+        }
+    }
+
+    public TodoEntry createTodo(TodoEntry newEntry) {
+        // Save our entry to the database and return the saved entry
+        TodoEntry savedEntry = this.todoRepository.save(newEntry);
+
+        return savedEntry;
+    }
+
     public TodoEntry updateTodo(long id, TodoEntry changes) throws InvalidIdException {
         // Get our to-do entry from the database
         Optional<TodoEntry> originalEntry = this.todoRepository.findById(id);
