832bab715d66d3c8d10fe8338d31681d422b7ec4
jonshadow69 <jonathanchuka@gmail.com>
2020-10-20 14:13:49 -0500

    refactored

--- backend/src/main/java/com/xpanxion/todo/TodoBackendApplication.java
+++ backend/src/main/java/com/xpanxion/todo/TodoBackendApplication.java
@@ -1,12 +1,11 @@
 package com.xpanxion.todo;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
 public class TodoBackendApplication {
 	// This allows Spring Boot to host itself via spring-boot:run, acting like a Tomcat server
 	public static void main(String[] args) {
+
 		SpringApplication.run(TodoBackendApplication.class, args);
 	}
 }
--- backend/src/main/java/com/xpanxion/todo/services/TodoEntryService.java
+++ backend/src/main/java/com/xpanxion/todo/services/TodoEntryService.java
@@ -1,11 +1,12 @@
 package com.xpanxion.todo.services;
 
 import com.xpanxion.todo.domain.TodoEntry;
-import com.xpanxion.todo.exceptions.InvalidIdException;
 import com.xpanxion.todo.repositories.TodoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -13,8 +14,38 @@ public class TodoEntryService {
     @Autowired
     TodoRepository todoRepository;
 
-    public TodoEntry updateTodo(long id, TodoEntry changes) throws InvalidIdException {
-        // Get our to-do entry from the database
+    public List getTodos() {
+        Sort sortByCreatedAt = Sort.by(Sort.Direction.DESC, "createdAt");
+
+        // Return all found results
+        return todoRepository.findAll(sortByCreatedAt);
+
+    }
+    public TodoEntry postTodo(TodoEntry todo) throws com.xpanxion.todo.exceptions.InvalidException {
+        TodoEntry savedEntry = todoRepository.save(todo);
+        return savedEntry;
+
+    }
+    public TodoEntry getTodoById(long id) throws com.xpanxion.todo.exceptions.InvalidException {
+
+
+        Optional<TodoEntry> originalEntry = todoRepository.findById(id);
+
+        // Check if we found an entry
+        if (originalEntry.isPresent()) {
+            // Use the entry's data
+            TodoEntry entryData = originalEntry.get();
+
+            // Return the to-do entry inside a 200 OK response
+            return entryData;
+        } else {
+            // Return a 404 Not Found status, since we didn't find the entry
+            throw new com.xpanxion.todo.exceptions.InvalidException();
+        }
+
+    }
+
+    public TodoEntry updateTodo ( long id, TodoEntry changes) throws com.xpanxion.todo.exceptions.InvalidException {
         Optional<TodoEntry> originalEntry = this.todoRepository.findById(id);
 
         // Check if we found an entry
@@ -22,32 +53,60 @@ public class TodoEntryService {
             // Use the entry's data
             TodoEntry entryData = originalEntry.get();
 
+            // Update the entry
             this.updateEntry(entryData, changes);
-
             // Save the updated version to the database
             TodoEntry updatedTodo = this.todoRepository.save(entryData);
 
             // Return the updated full entry
             return updatedTodo;
         } else {
-            // Throwing an invalid ID exception as the ID could not be found in the database
-            throw new InvalidIdException();
+            // throwing invalid id exception
+            throw new com.xpanxion.todo.exceptions.InvalidException();
         }
     }
 
-    private void updateEntry(TodoEntry entryData, TodoEntry changes) {
-        // Update the entry
-        if(changes.getTitle() != null) {
+    private void updateEntry (TodoEntry entryData, TodoEntry changes){
+        if (changes.getTitle() != null) {
+
             entryData.setTitle(changes.getTitle());
+
         }
-        if(changes.getDescription() != null) {
+        if (changes.getDescription() != null) {
+
             entryData.setDescription(changes.getDescription());
+
+
         }
-        if(changes.getCompleted() != null) {
-            entryData.setCompleted(changes.getCompleted());
-        }
-        if(changes.getCreatedAt() != null) {
+        if (changes.getCreatedAt() != null) {
+
             entryData.setCreatedAt(changes.getCreatedAt());
+
+
+        }
+        if (changes.getCompleted() != null) {
+
+            entryData.setCompleted(changes.getCompleted());
+
+
         }
     }
-}
+
+
+    public void deleteTodo ( long id) throws com.xpanxion.todo.exceptions.InvalidException {
+        Optional<TodoEntry> originalEntry = todoRepository.findById(id);
+
+// Check if we found an entry
+        if (originalEntry.isPresent()) {
+            // Delete the entry we found by ID
+
+            // Return a 204 OK No Content status, we deleted it
+            this.todoRepository.deleteById(id);
+
+
+        } else {
+            // Return a 404 Not Found status, since we didn't find the entry
+            throw new com.xpanxion.todo.exceptions.InvalidException();
+        }
+
+    }}
